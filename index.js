557debe0587aa6d6076b043267d97556e4015de9
David <behoiandavid@gmail.com>
2025-07-04 21:34:57 +0400

    Явный вызов итератора

--- index.js
+++ index.js
@@ -241,11 +241,12 @@
 
 function camelize(str) {
 
-str.split('')
+    return str
+        .split('-')
+        .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1))
+        .join('')
 
 }
-
-
-camelize("background-color") == 'backgroundColor';
-camelize("list-style-image") == 'listStyleImage';
-camelize("-webkit-transition") == 'WebkitTransition';
+console.log(camelize("background-color"));   // "backgroundColor"
+console.log(camelize("list-style-image"));   // "listStyleImage"
+console.log(camelize("-webkit-transition")); // "WebkitTransition
